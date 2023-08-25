0639e7cce5f6aebbfcdffcd694c3c49032ac18e4
John Carlo <johncarlolabanero@gmail.com>
2023-08-25 21:31:32 +0800

    untracked conditional statements

--- conditional_statements.py
+++ conditional_statements.py
@@ -5,4 +5,16 @@ if isMale:
 else:
     print("You are a female")
 
+x = 9
+y = 2
+summation = x+y
+difference = x-y
+product = x*y
+qoutient = x/y
+remainder = x % y
+exponential = x**y
+floor = x//y
+
+print(f"sum={summation} diff={difference} prod={product} quo={qoutient} rem={remainder} exp={exponential} flo={floor}")
+
 # operators are + - * / % ** //
